8a076d750ee2a76cdc1e1d82348dcb83bd80ef68
Anujithaaa <86418484+Anujithaaa@users.noreply.github.com>
2023-02-22 12:23:16 +0530

    ngFor directive

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+
+products = [
+  {id:1, name: 'Product 1', price: 100, color: 'blue', available: 'Yes', image: '/assets/products/product-image-1.jpeg'},
+  {id:2, name: 'Product 2', price: 200, color: 'black', available: 'No', image: '/assets/products/product-image-2.jpg'},
+  {id:3, name: 'Product 3', price: 300, color: 'red', available: 'Yes', image: '/assets/products/product-image-3.jpg'}, 
+  {id:4, name: 'Product 4', price: 400, color: 'white', available: 'No', image: '/assets/products/product-image-4.jpg'},
+  {id:5, name: 'Product 5', price: 500, color: 'green', available: 'Yes', image: '/assets/products/product-image-5.jpg'},
+  {id:6, name: 'Product 6', price: 600, color: 'yellow', available: 'No', image: '/assets/products/product-image-6.jpg'},
+];  
+ 
+}
